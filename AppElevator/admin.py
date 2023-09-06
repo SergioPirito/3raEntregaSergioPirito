b8652887ad56677bea9bf7d39b6320460baca57d
SergioPirito <sergio.pirito@gmail.com>
2023-09-05 21:04:50 -0300

    cambios

--- AppElevator/admin.py
+++ AppElevator/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import autor
+from .models import productora_audiovisual
+from .models import Proyecto
+from .models import productor
 # Register your models here.
+admin.site.register(autor)
+admin.site.register(productora_audiovisual)
+admin.site.register(Proyecto)
+admin.site.register(productor)
